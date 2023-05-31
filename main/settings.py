11488114acbc9cd33894b1c7610d2744f07c1f37
Amjad Projects <aakhan1572@yahoo.com>
2023-05-31 22:31:29 +0530

    first commit

--- main/settings.py
+++ main/settings.py
@@ -3,18 +3,8 @@ from pathlib import Path
 #from decouple import config
 import os
 import environ
-
-
 env = environ.Env()
-
-# Set the project base directory
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Take environment variables from .env file
-#environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, True)
